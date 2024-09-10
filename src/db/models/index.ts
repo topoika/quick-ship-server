638586f871fdf39bc3f5b06b5ffef86873ea189a
topoika <topoikatampul@gmail.com>
2024-09-10 19:13:18 +0300

    Update package.json and tsconfig.json

--- src/db/models/index.ts
+++ src/db/models/index.ts
@@ -6,36 +6,38 @@ const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
-
 const db: any = {};
 
 let sequelize: any;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], {
-    dialect: config.dialect,
-    ...config,
-  });
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, {
-    dialect: config.dialect,
-    ...config,
-  });
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
+
 fs.readdirSync(__dirname)
   .filter((file: string) => {
     return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".ts"
+      file.indexOf(".") !== 0 &&
+      file !== basename &&
+      file.slice(-3) === ".js" &&
+      file.indexOf(".test.js") === -1
     );
   })
   .forEach((file: any) => {
-    const model = require(path.join(__dirname, file)).default(
+    const model = require(path.join(__dirname, file))(
       sequelize,
       Sequelize.DataTypes
     );
     db[model.name] = model;
   });
 
-Object.keys(db).forEach((modelName) => {
+Object.keys(db).forEach((modelName: any) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
@@ -44,4 +46,4 @@ Object.keys(db).forEach((modelName) => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-export default db;
+module.exports = db;
